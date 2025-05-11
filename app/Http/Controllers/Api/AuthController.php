a0f1b78dffacd1ee3d940949d288125d45d64a2e
Lordza <d2022004860@unifei.edu.br>
2025-05-11 00:21:56 -0300

    Init

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ApiLoginRequest;
+use App\Models\User;
+use App\Traits\ApiResponses;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+    public function login(ApiLoginRequest $request) {
+        $credentials = $request->validate([
+            'email' => ['email', 'string', 'required'],
+            'password' => ['min:8', 'string', 'required'],
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return $this->error("Invalid credentials", 401);
+        }
+
+        $user = User::firstWhere("email", $request->email);
+        return $this->success("Authenticated", ['token' => $user->createToken('API token for ' . $user->email)->plainTextToken]);
+    }
+
+    public function logout(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->ok('',);
+    }
+
+    public function register(Request $request) {
+        $userData = $request->validate([
+            'name' => ['string', 'required'],
+            'email' => ['email', 'string', 'required'],
+            'password' => ['min:8', 'string', 'required'],
+        ]);
+
+        $userData['password'] = bcrypt($userData['password']);
+
+        $user = User::create($userData);
+
+        return $this->ok(['register']);
+    }
+}
