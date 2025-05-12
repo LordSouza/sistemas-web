ee583d25c10ae50f0779916c971197dfb0d1dc36
lords <d2022004860@unifei.edu.br>
2025-05-12 19:44:32 -0300

    laravel pint

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,33 +6,38 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ApiLoginRequest;
 use App\Models\User;
 use App\Traits\ApiResponses;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     use ApiResponses;
-    public function login(ApiLoginRequest $request) {
+
+    public function login(ApiLoginRequest $request)
+    {
         $credentials = $request->validate([
             'email' => ['email', 'string', 'required'],
             'password' => ['min:8', 'string', 'required'],
         ]);
 
-        if (!Auth::attempt($credentials)) {
-            return $this->error("Invalid credentials", 401);
+        if (! Auth::attempt($credentials)) {
+            return $this->error('Invalid credentials', 401);
         }
 
-        $user = User::firstWhere("email", $request->email);
-        return $this->success("Authenticated", ['token' => $user->createToken('API token for ' . $user->email)->plainTextToken]);
+        $user = User::firstWhere('email', $request->email);
+
+        return $this->success('Authenticated', ['token' => $user->createToken('API token for '.$user->email)->plainTextToken]);
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
         $request->user()->currentAccessToken()->delete();
 
-        return $this->ok('',);
+        return $this->ok('');
     }
 
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         $userData = $request->validate([
             'name' => ['string', 'required'],
             'email' => ['email', 'string', 'required'],
--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -4,23 +4,25 @@ namespace App\Traits;
 
 trait ApiResponses
 {
-    protected function ok($message, $data = []) {
+    protected function ok($message, $data = [])
+    {
         return $this->success($message, $data, 200);
     }
+
     protected function success($message, $data = [], $statusCode = 200)
     {
         return response()->json([
-            "data" => $data,
-            "message" => $message,
-            "status" => $statusCode
+            'data' => $data,
+            'message' => $message,
+            'status' => $statusCode,
         ], $statusCode);
     }
 
     protected function error($message, $statusCode)
     {
         return response()->json([
-            "message" => $message,
-            "status" => $statusCode
+            'message' => $message,
+            'status' => $statusCode,
         ], $statusCode);
     }
 }
