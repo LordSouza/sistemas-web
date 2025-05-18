098c25b1fd5a07a453fda1c39e727a70ffb18943
Letícia Santos <leticiavsantoos@gmail.com>
2025-05-18 15:39:24 -0300

    add pasta front-end e inicia layout login

---
+++ front-end/src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <div>
+      <section>
+        <img src="/logo.png" alt="Logo do projeto Flora Alert" className="w-[500px]"/>
+      </section>
+      <section></section>
+    </div>
+  );
+}
